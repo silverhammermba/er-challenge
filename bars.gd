e5bac98c4215350975ed9c7adff3ced7ef26ee2f
Maxwell Anselm <manselm@lutron.com>
2025-09-04 19:28:17 -0400

    add GPL license

--- bars.gd
+++ bars.gd
@@ -1,3 +1,8 @@
+# This file is part of ER-Challenge.
+# ER-Challenge is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# ER-Challenge is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with ER-Challenge. If not, see <https://www.gnu.org/licenses/>.
+
 class_name Bars extends Control
 
 const Data = preload("data.gd")
